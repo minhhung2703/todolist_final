11b08ea835066bc33f7141c05443bf66d5c181f1
MinhHung2703 <nguyenminhhung27032002@gmail.com>
2023-01-14 21:57:24 +0700

    fixed page

--- login.js
+++ login.js
@@ -1,5 +1,4 @@
 $('#form').find('input, textarea').on('keyup blur focus', function (e) {
-
     var $this = $(this),
         label = $this.prev('label');
 
@@ -28,7 +27,6 @@ $('#form').find('input, textarea').on('keyup blur focus', function (e) {
 });
 
 $('.tab a').on('click', function (e) {
-
     e.preventDefault();
 
     $(this).parent().addClass('active');
@@ -41,3 +39,54 @@ $('.tab a').on('click', function (e) {
     $(target).fadeIn(800);
 
 });
+
+document.getElementById("loginForm").addEventListener("submit", (function (e) {
+    e.preventDefault();
+    const email = document.getElementById("emailLogin").value;
+    const password = document.getElementById("pLogin").value;
+    console.log({ email, password });
+    fetch("https://dev.thanqminh.com:3001/auth/sign_in", {
+        method: "POST",
+        body: JSON.stringify({ email, password }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(async (response) => {
+        if (response.status != 200) {
+            return window.alert("Login failed");;
+        }
+        const data = await response.json();
+        window.localStorage.setItem("hAccessToken", response.headers.get("access-token"));
+        window.localStorage.setItem("client", response.headers.get("client"));
+        window.localStorage.setItem("uid", response.headers.get("uid"));
+        window.localStorage.setItem("userData", JSON.stringify(data));
+        window.location.href = "todolist.html";
+    }).catch((err) => {
+        console.log("Login failed", err);
+        window.alert("Login failed");
+    })
+}));
+
+
+document.getElementById("signupForm").addEventListener("submit", (function (e) {
+    e.preventDefault();
+    const email = document.getElementById("emailSignup").value;
+    const password = document.getElementById("passwordSignup").value;
+    console.log({ email, password });
+    fetch("https://dev.thanqminh.com:3001/auth", {
+        method: "POST",
+        body: JSON.stringify({ email, password }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(async (response) => {
+        if (response.status != 200) {
+            return window.alert("Signup failed");;
+        }
+        await response.json();
+        window.alert("Signup suceesfully");
+    }).catch((err) => {
+        console.log("Login failed", err);
+        window.alert("Signup failed");
+    })
+}));
--- todolist.js
+++ todolist.js
@@ -1,13 +1,42 @@
 window.addEventListener('load', () => {
+    function fetchWithCredential(url, method, data) {
+        const credential = window.localStorage.getItem('hAccessToken');
+        const client = window.localStorage.getItem("client");
+        const uid = window.localStorage.getItem("uid");
+        return new Promise((resolve, reject) => {
+            fetch("https://dev.thanqminh.com:3001" + url, Object.assign({
+                method,
+                headers: {
+                    "Content-Type": "application/json",
+                    "access-token": credential,
+                    client,
+                    uid,
+                },
+
+            }, data ? { body: data ? JSON.stringify(data) : null, } : {})).then((res) => res.json()).then((response) => {
+                resolve(response);
+            }).catch((err) => reject(err))
+        });
+    }
+
     const form = document.querySelector("#new-task-form");
     const input = document.querySelector("#new-task-input");
     const list_el = document.querySelector("#tasks");
 
-    form.addEventListener('submit', (e) => {
+    form.addEventListener('submit', async (e) => {
         e.preventDefault();
-
         const task = input.value;
+        try {
+            const addTaskResponse = await fetchWithCredential("/task_lists", "POST", { name: task });
+            console.log(addTaskResponse);
+            addUI(addTaskResponse.name, addTaskResponse.id);
+            setSuccessMessage(addTaskResponse.name)
+            clearFormData();
+            getData(); // reload table 
+        } catch { };
+    });
 
+    function addUI(task, id) {
         const task_el = document.createElement('div');
         task_el.classList.add('task');
 
@@ -44,7 +73,7 @@ window.addEventListener('load', () => {
 
         input.value = '';
 
-        task_edit_el.addEventListener('click', (e) => {
+        task_edit_el.addEventListener('click', async (e) => {
             if (task_edit_el.innerText.toLowerCase() == "edit") {
                 task_edit_el.innerText = "Save";
                 task_input_el.removeAttribute("readonly");
@@ -52,11 +81,33 @@ window.addEventListener('load', () => {
             } else {
                 task_edit_el.innerText = "Edit";
                 task_input_el.setAttribute("readonly", "readonly");
+                console.log(task_input_el.value);
+                try {
+                    await fetchWithCredential("/task_lists/" + id, "PATCH", { name: task_input_el.value });
+                } catch (err) {
+                    list_el.removeChild(task_el);
+                }
             }
         });
 
-        task_delete_el.addEventListener('click', (e) => {
-            list_el.removeChild(task_el);
+        task_delete_el.addEventListener('click', async (e) => {
+            try {
+                await fetchWithCredential("/task_lists/" + id, "DELETE");
+                list_el.removeChild(task_el);
+            } catch (err) {
+                list_el.removeChild(task_el);
+            }
         });
-    });
+    }
+
+    async function get() {
+        try {
+            const response = await fetchWithCredential("/task_lists", "GET");
+            response.map((item) => {
+                addUI(item.name, item.id);
+            })
+        } catch { }
+    }
+
+    get();
 })
